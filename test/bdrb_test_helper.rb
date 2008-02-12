84f53d5ac4f7f85bd3ab729924ed257ae33ed7bf
gnufied <mail@gnufied.org>
2008-02-12 15:33:13 +0530

    modified helper

--- test/bdrb_test_helper.rb
+++ test/bdrb_test_helper.rb
@@ -3,24 +3,20 @@ require "mocha"
 require "test/spec"
 require "active_record" 
 require "active_support" 
+require "yaml"
+require "erb"
 
 RAILS_HOME = File.expand_path(File.join(File.dirname(__FILE__) + "/../../../.."))
 PACKET_APP = RAILS_HOME + "/vendor/plugins/backgroundrb"
 WORKER_ROOT = RAILS_HOME + "/lib/workers"
 SERVER_LOGGER = RAILS_HOME + "/log/backgroundrb_server.log"
 
-["server","framework","lib"].each { |x| $LOAD_PATH.unshift(PACKET_APP + "/#{x}")}
+["server","server/lib","lib"].each { |x| $LOAD_PATH.unshift(PACKET_APP + "/#{x}")}
 $LOAD_PATH.unshift(WORKER_ROOT)
 
 # require "#{PACKET_APP}/server/master_worker"
 require "packet"
-require "meta_worker"
-require "cron_trigger"
-require "trigger"
-require "log_worker"
-require "yaml"
-require "erb"
-
+require "backgroundrb_server" 
 
 # there should be a way to stub out reactor loop of bdrb
 
