a2f38c930c97aadfe9b9a03f5f118ead95553464
Alex P <gitorious@peuchert.de>
2008-03-08 13:19:29 +0100

    [BUG] ARGV was not passed into config.rb

--- lib/backgroundrb/bdrb_config.rb
+++ lib/backgroundrb/bdrb_config.rb
@@ -2,7 +2,7 @@ module BackgrounDRb
 end
 
 class BackgrounDRb::Config
-  def self.parse_cmd_options
+  def self.parse_cmd_options(argv)
     require 'optparse'
     options = { :environment => (ENV['RAILS_ENV'] || "development").dup }
 
@@ -19,7 +19,7 @@ class BackgrounDRb::Config
       opts.separator ""
       opts.on("-v","--version",
               "Show version.") { $stderr.puts "1.0.3"; exit }
-    end.parse!
+    end.parse!(argv)
 
     ENV["RAILS_ENV"] = options[:environment]
     RAILS_ENV.replace(options[:environment]) if defined?(RAILS_ENV)
