a15e1916751fdd4600527b922fcd1ad93d2d923f
gnufied <gethemant@gmail.com>
2008-08-15 08:35:59 +0530

    check in some testcases for environment thingis

---
+++ test/client/test_bdrb_config.rb
@@ -0,0 +1,19 @@
+require File.join(File.dirname(__FILE__) + "/../bdrb_test_helper")
+require File.join(File.dirname(__FILE__) + "/../bdrb_client_test_helper")
+
+context "For BackgrounDRb config" do
+  conf_file = File.join(File.dirname(__FILE__),"backgroundrb.yml")
+  specify "should setup correct environment from cmd options" do
+    BackgrounDRb::Config.parse_cmd_options(["-e", "production"])
+    BackgrounDRb::Config.read_config(conf_file)
+    ENV["RAILS_ENV"].should == "production"
+    RAILS_ENV.should == "production"
+  end
+
+  specify "should setup correct environment from conf file" do
+    BackgrounDRb::Config.parse_cmd_options([])
+    BackgrounDRb::Config.read_config(conf_file)
+    ENV["RAILS_ENV"].should == "production"
+    RAILS_ENV.should == "production"
+  end
+end
