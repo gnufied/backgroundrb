6e3e688cfdeb134f06d9bbb323a194609b427914
jdwyer <jdwyah@gmail.com>
2009-04-20 18:07:20 -0400

    steve BDRB regex fix

--- server/lib/bdrb_server_helper.rb
+++ server/lib/bdrb_server_helper.rb
@@ -7,7 +7,7 @@ module BackgrounDRb
         return Marshal.load(data)
       rescue
         error_msg = $!.message
-        if error_msg =~ /^undefined\ .+\ ([A-Z].+)/
+        if error_msg =~ /^undefined\ .+\ ([A-Z][^:]+)/
           file_name = $1.underscore
           begin
             require file_name
